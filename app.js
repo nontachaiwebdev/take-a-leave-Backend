f51703b9273ae285c5c8c0738e5d60804b81d2e1
nontachaiwebdev <sutininter@gmail.com>
2018-05-24 21:59:54 +0700

    feat(script): add start script

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import cors from 'cors'
 import express from 'express'
+import bodyParser from 'body-parser'
 import { CORS_OPTIONS, SERVER_PORT } from './config/server'
 import { NODE_ENV } from './config/env'
 
 const app = express()
 
+// use cors middleware
 app.use(cors(CORS_OPTIONS))
+
+// use bodyParser middleware
+app.use(bodyParser.json({limit: '15mb'}))
+app.use(bodyParser.urlencoded({extended: false}))
+
+
 
 
 if (NODE_ENV !== 'test') {
